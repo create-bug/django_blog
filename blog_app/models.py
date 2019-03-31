92b41bd42afee38e4b80b100cadca5b4bd18a6d0
createbug <probloging@protonmail.com>
2019-03-31 12:32:56 +0800

    Comments models

--- blog_app/models.py
+++ blog_app/models.py
@@ -17,3 +17,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80) 
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True) # 关闭不当评论
