d2f8100303ecd7aad316284c755d7723aae8415c
createbug <probloging@protonmail.com>
2019-03-31 11:27:03 +0800

    注册登出登录链接

--- account/urls.py
+++ account/urls.py
@@ -5,6 +5,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.test_login, name='test_login'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout', auth_views.LogoutView.as_view(), name='logiut'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
 ]
