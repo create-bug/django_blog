51bb2c67e346397b6834e9eb9b0b2031423b07f1
createbug <probloging@protonmail.com>
2019-03-31 08:38:16 +0800

    first commit

---
+++ account/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, HttpResponse
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
+# Create your views here.
+
+def user_login(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('登录成功')
+                else:
+                    return HttpResponse('登录失败')
+            else:
+                return HttpResponse('登录失败')
+    else:
+        form = LoginForm()
+
+    return render(request, 'templates/account/login', {'form':form})
